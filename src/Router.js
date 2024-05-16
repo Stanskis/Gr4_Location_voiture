842956e3255d51fe74094694503af81dfd3939f9
Yassin-Rahmani <yassin.rahmani@gmail.com>
2024-05-16 09:46:10 +0200

    maj

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,8 @@
 import App from "./App.js";
 import HomeScreen from "./screens/HomeScreen.js";
 import CatalogueScreen from "./screens/CatalogueScreen.js";
+import LoginScreen from "./screens/LoginScreen.js";
+import CreateAccountScreen from "./screens/CreateAccountScreen.js";
 
 export default class Router{
 
@@ -18,7 +20,7 @@ export default class Router{
             e.preventDefault();
             console.log(link.href);
             history.pushState("", "", link.href);
-            this.navigate();
+            this.navigate(e);
         }
     }
 
@@ -41,7 +43,11 @@ export default class Router{
         }
         else if(location.pathname == "/login"){
             document.title = "Location Voiture - Login";
-            page = `<h1>Login</h1>`;
+            page = new LoginScreen();
+        }
+        else if(location.pathname == "/create-account"){
+            document.title = "Location Voiture - Create account";
+            page = new CreateAccountScreen();
         }
         else {
             page = `<h1>Page 404</h1>`;
